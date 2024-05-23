08edbdd4447550dae7cd67370b5bac2b4a635b53
giovanni2299 <giovanni-sorrentino@hotmail.it>
2024-05-23 14:50:33 +0200

    ADD: comment and import movie in pagecontroller

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -1,12 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index() {
+
+        $movie = Movie::all();
+        dd($movie);
+
         return view('welcome');
     }
 }
